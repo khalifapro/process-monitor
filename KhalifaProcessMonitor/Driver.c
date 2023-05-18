5327f91d4c6d0f5a3d8cb15a376a7debfef48696
Ayman <enkhalifapro@gmail.com>
2023-05-18 04:28:16 +0300

    init commit
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

---
+++ KhalifaProcessMonitor/Driver.c
@@ -0,0 +1,30 @@
+#include <ntddk.h>
+#include <wdf.h>
+#include <wdm.h>
+
+DRIVER_INITIALIZE DriverEntry;
+
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT     DriverObject,
+    _In_ PUNICODE_STRING    RegistryPath
+)
+{
+    // NTSTATUS variable to record success or failure
+    NTSTATUS status = STATUS_SUCCESS;
+
+    // Allocate the driver configuration object
+    WDF_DRIVER_CONFIG config;
+
+    // Print "Hello World" for DriverEntry
+    DbgPrint("Hello, world! khalifa 222\n");
+
+    // Finally, create the driver object
+    status = WdfDriverCreate(DriverObject,
+        RegistryPath,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &config,
+        WDF_NO_HANDLE
+    );
+    return status;
+}
